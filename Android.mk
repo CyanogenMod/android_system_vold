4e73e325831464454a9f37f555c5ac337081c94e
cicada <pking@edencomputing.com>
2011-03-02 10:48:16 -0700

    vold: allow override of MAX_PARTITIONS for devices with > 4 partitions
    Change-Id: Iec4de6e7e5ad52a6dfa45952bb19104f500d9c58a

--- Android.mk
+++ Android.mk
@@ -32,6 +32,10 @@ common_shared_libraries := \
 
 include $(CLEAR_VARS)
 
+ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
+LOCAL_CFLAGS += -DVOLD_MAX_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
+endif
+
 LOCAL_MODULE := libvold
 
 LOCAL_SRC_FILES := $(common_src_files)
@@ -57,6 +61,11 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := $(common_c_includes)
 
 LOCAL_CFLAGS := -Werror=format
+
+ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
+LOCAL_CFLAGS += -DVOLD_MAX_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
+endif
+
 
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 
