b240c858bb81b0d0e61a74c49579b8eba14982a8
kallt_kaffe <kallt_kaffe@apedroid.com>
2011-10-13 14:38:29 +0200

    vold: add ro.vold.umsdirtyratio property
    This allows control of what /proc/sys/vm/dirty_ratio is used when USB mounting.
    Some platforms like the HTC Desire (bravo) get a _HUGE_ performance boost when
    writing to the sdcard when USB mounting if this is set to 20 (which is the default)
    on the HTC Desire.
    
    This addresses this issue: http://code.google.com/p/cyanogenmod/issues/detail?id=2949
    
    Basicly it makes it possible to undo or control the effect of this commit:
    https://github.com/CyanogenMod/android_system_vold/commit/a28056b38275003895ff5d9576681aca01544822
    
    Change-Id: I86b7bbd02695fb0c19f39448714a219257bf806f

--- VolumeManager.cpp
+++ VolumeManager.cpp
@@ -35,6 +35,7 @@
 
 #include <cutils/fs.h>
 #include <cutils/log.h>
+#include <cutils/properties.h>
 
 #include <selinux/android.h>
 
@@ -174,9 +175,12 @@ VolumeManager::VolumeManager() {
     mBroadcaster = NULL;
     mUmsSharingCount = 0;
     mSavedDirtyRatio = -1;
-    // set dirty ratio to 0 when UMS is active
-    mUmsDirtyRatio = 0;
     mVolManagerDisabled = 0;
+
+    // set dirty ratio to ro.vold.umsdirtyratio (default 0) when UMS is active
+    char dirtyratio[PROPERTY_VALUE_MAX];
+    property_get("ro.vold.umsdirtyratio", dirtyratio, "0");
+    mUmsDirtyRatio = atoi(dirtyratio);
 }
 
 VolumeManager::~VolumeManager() {
