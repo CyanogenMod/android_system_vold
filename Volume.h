a2545aa99a7bea77b7ccae0af2217314e350c9f1
Pawit Pornkitprasan <p.pawit@gmail.com>
2011-12-03 15:25:33 +0700

    Allow mounting of multiple volumes via mass storage (vold part)
    commit 662006fe5419803938790a164cb1ba73b05e3f41
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Sat Dec 3 13:31:46 2011 +0700
    
        Extracted primary storage detection into isPrimaryStorage()
    
        Change-Id: Ia76ace183869283e7e6563ea8d5eb919283ffe55
    
    commit 0303f8b0722bfac3c1b0a522f42c921e3ed4666a
    Author: Genokolar <genokolar@gmail.com>
    Date:   Sat Oct 1 21:34:00 2011 +0800
    
        Add custom second lun number support
    
        like u8800 have different second lun number
        use TARGET_USE_CUSTOM_SECOND_LUN_NUM options to custom lun number
    
        Change-Id: I36f8c92f1f8041efeaa0304795a11eb1cc49e5e1
        (cherry picked from commit 24860389177df3aa839a8eb77d79608b15e0af8d)
    
    commit 4b99cd585d12fa47b2f8d99cde430f37d2fe7293
    Author: Alex Zepeda <alex@inferiorhumanorgans.com>
    Date:   Mon Jul 18 10:55:33 2011 -0700
    
        Use a list of possible lun paths instead of a hardcoded one.
    
        This will easily play nice with kernels that use gadget drivers and
        function drivers (ex: tethering support doesn't require a new vold).
    
        Change-Id: I71796114501cf2d64dea5fd077ac5baf54873af4
    
    commit 1f822e51cde73bc0d8e1b575848614f696534a88
    Author: mik_os <kiril.mik.os@gmail.com>
    Date:   Mon Feb 14 16:10:36 2011 +0200
    
        Added ability to use custom lun path.
    
        Change-Id: If39e445383c0ea0d64556df5708de80c06056174
        (cherry picked from commit 5edf3ce983ddd3ffed01c398244493ab962e3304)
    
    commit baa2d75929be1bebb1e04d93044d7718460d5b14
    Author: Josh Stone <cuviper@gmail.com>
    Date:   Sat Jan 22 11:52:01 2011 -0800
    
        vold: cleanupAsec should only do work for EXTERNAL_STORAGE
    
        For devices like DINC which have both emmc and sdcard storage, only one
        of these is used for ASEC mounts.  When we're unmounting volumes, we
        need to discriminate which is which before forcing ASEC closures.
    
        Change-Id: I94efda36414bb0eb2c4c4e31891ce4f318582a8e
        (cherry picked from commit 61272bf32aff5515aa6f48acb2e8ea7e95b47aa3)
    
    commit 73aeea8a3c834d7d822edc088f19934843511779
    Author: Tony Layher <layhertony@gmail.com>
    Date:   Sun Jan 2 19:17:39 2011 -0500
    
        Fix build break on Vold, have to remove uneeded code
    
        Change-Id: Ie594367d3675cf25cf4c86703aaa01055a9892a6
        (cherry picked from commit f04648bda6bf58391f46fc8694718e5878e4abb9)
    
    commit 332a373eaadb487f069fd86f0876abade84f7189
    Author: Tony Layher <layhertony@gmail.com>
    Date:   Sun Jan 2 14:48:26 2011 -0500
    
        Forward Port of vold changes for having 2 UMS devices and use only EXTERNAL_STORAGE for ASEC
    
        Change-Id: Ia26b4251226103441afb2656ea411c68525974c9
    
    Change-Id: I5695a0441a6672e9c1c2f2aef95c38f0b08a7145

--- Volume.h
+++ Volume.h
@@ -71,6 +71,7 @@ public:
     const char *getLabel() { return mLabel; }
     const char *getMountpoint() { return mMountpoint; }
     int getState() { return mState; }
+    bool isPrimaryStorage();
 
     virtual int handleBlockEvent(NetlinkEvent *evt);
     virtual dev_t getDiskDevice();
