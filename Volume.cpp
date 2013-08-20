89a65d07e49b9d25bdd3a6e00687237f28dfe455
Steve Kondik <shade@chemlab.org>
2013-08-19 17:16:39 -0700

    vold: Format filesystems as exfat, if already exfat.
    Change-Id: I1474fa3d3264e6466c2f74e91f16bd2515f50d06

--- Volume.cpp
+++ Volume.cpp
@@ -281,6 +281,8 @@ int Volume::createDeviceNode(const char *path, int major, int minor) {
 
 int Volume::formatVol() {
 
+    char* fstype = NULL;
+
     if (getState() == Volume::State_NoMedia) {
         errno = ENODEV;
         return -1;
@@ -332,7 +334,12 @@ int Volume::formatVol() {
         SLOGI("Formatting volume %s (%s)", getLabel(), devicePath);
     }
 
-    if (Fat::format(devicePath, 0)) {
+    fstype = getFsType((const char*)devicePath);
+    if (strcmp(fstype, "exfat") == 0) {
+        if (Exfat::format(devicePath)) {
+            SLOGE("Failed for format (%s) as exfat", strerror(errno));
+        }
+    } else if (Fat::format(devicePath, 0)) {
         SLOGE("Failed to format (%s)", strerror(errno));
         goto err;
     }
