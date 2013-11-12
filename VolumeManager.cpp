f5fde517158240bbcc9358e46a42c9c14e87006a
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-11-12 14:58:59 +0700

    vold: Add selinux support to ext4
    Squashed commit of the following:
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    vold: mount ext4 with forced SELinux context
    
    Always mount voldmanaged ext4 partitions with the SELinux sdcard_external
    context. For other file systems, this is done via the genfs_context,
    but since ext4 supports SELinux natively, it must be set here.
    Change-Id: I1364f37018b25d79b1826c85849def64e868d72f
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    vold: force SELinux context only for sdcard
    
    Google uses this code to mount ASEC as well and we shouldn't force
    their SELinux contexts
    Change-Id: I873af4dc2309d3d0942ce466b8acf8158abb85ae
    
    Author: Jorge Ruesga <jorge@ruesga.com>
    vold: pass selinux context to ext4 mount (remount ro)
    Change-Id: Ic0bb314b30e42489c45caec29d35d6896c9849eb
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Change-Id: I1364f37018b25d79b1826c85849def64e868d72f

--- VolumeManager.cpp
+++ VolumeManager.cpp
@@ -566,7 +566,7 @@ int VolumeManager::createAsec(const char *id, unsigned int numSectors, const cha
 
         int mountStatus;
         if (usingExt4) {
-            mountStatus = Ext4::doMount(dmDevice, mountPoint, false, false, false);
+            mountStatus = Ext4::doMount(dmDevice, mountPoint, false, false, false, false);
         } else {
             mountStatus = Fat::doMount(dmDevice, mountPoint, false, false, false, ownerUid, 0, 0000,
                     false);
@@ -785,7 +785,7 @@ int VolumeManager::finalizeAsec(const char *id) {
 
     int result = 0;
     if (sb.c_opts & ASEC_SB_C_OPTS_EXT4) {
-        result = Ext4::doMount(loopDevice, mountPoint, true, true, true);
+        result = Ext4::doMount(loopDevice, mountPoint, true, true, true, false);
     } else {
         result = Fat::doMount(loopDevice, mountPoint, true, true, true, 0, 0, 0227, false);
     }
@@ -854,7 +854,8 @@ int VolumeManager::fixupAsecPermissions(const char *id, gid_t gid, const char* f
     int ret = Ext4::doMount(loopDevice, mountPoint,
             false /* read-only */,
             true  /* remount */,
-            false /* executable */);
+            false /* executable */,
+            false /* sdcard */);
     if (ret) {
         SLOGE("Unable remount to fix permissions for %s (%s)", id, strerror(errno));
         return -1;
@@ -916,7 +917,8 @@ int VolumeManager::fixupAsecPermissions(const char *id, gid_t gid, const char* f
     result |= Ext4::doMount(loopDevice, mountPoint,
             true /* read-only */,
             true /* remount */,
-            true /* execute */);
+            true /* execute */,
+            false /* sdcard */);
 
     if (result) {
         SLOGE("ASEC fix permissions failed (%s)", strerror(errno));
@@ -1353,7 +1355,7 @@ int VolumeManager::mountAsec(const char *id, const char *key, int ownerUid, bool
 
     int result;
     if (sb.c_opts & ASEC_SB_C_OPTS_EXT4) {
-        result = Ext4::doMount(dmDevice, mountPoint, readOnly, false, readOnly);
+        result = Ext4::doMount(dmDevice, mountPoint, readOnly, false, readOnly, false);
     } else {
         result = Fat::doMount(dmDevice, mountPoint, readOnly, false, readOnly, ownerUid, 0, 0222, false);
     }
