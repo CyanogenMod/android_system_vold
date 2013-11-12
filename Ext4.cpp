fbfbce0fef7159d27bce2b7df9a871352ff65dab
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-11-12 14:58:59 +0700

    vold: Add selinux support to ext4
    Squashed commit of the following:
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    vold: mount ext4 with forced SELinux context
    
    Always mount voldmanaged ext4 partitions with the SELinux sdcard_external
    context. For other file systems, this is done via the genfs_context,
    but since ext4 supports SELinux natively, it must be set here.
    Change-Id: I1364f37018b25d79b1826c85849def64e868d72f
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    vold: force SELinux context only for sdcard
    
    Google uses this code to mount ASEC as well and we shouldn't force
    their SELinux contexts
    Change-Id: I873af4dc2309d3d0942ce466b8acf8158abb85ae
    
    Author: Jorge Ruesga <jorge@ruesga.com>
    vold: pass selinux context to ext4 mount (remount ro)
    Change-Id: Ic0bb314b30e42489c45caec29d35d6896c9849eb
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Change-Id: I1364f37018b25d79b1826c85849def64e868d72f

--- Ext4.cpp
+++ Ext4.cpp
@@ -48,9 +48,10 @@ static char RESIZE2FS_PATH[] = "/system/bin/resize2fs";
 static char MKEXT4FS_PATH[] = "/system/bin/make_ext4fs";
 
 int Ext4::doMount(const char *fsPath, const char *mountPoint, bool ro, bool remount,
-        bool executable) {
+        bool executable, bool sdcard) {
     int rc;
     unsigned long flags;
+    const char *data = NULL;
 
     flags = MS_NOATIME | MS_NODEV | MS_NOSUID | MS_DIRSYNC;
 
@@ -58,12 +59,17 @@ int Ext4::doMount(const char *fsPath, const char *mountPoint, bool ro, bool remo
     flags |= (ro ? MS_RDONLY : 0);
     flags |= (remount ? MS_REMOUNT : 0);
 
-    rc = mount(fsPath, mountPoint, "ext4", flags, NULL);
+    if (sdcard) {
+        // Mount external volumes with forced context
+        data = "context=u:object_r:sdcard_external:s0";
+    }
+
+    rc = mount(fsPath, mountPoint, "ext4", flags, data);
 
     if (rc && errno == EROFS) {
         SLOGE("%s appears to be a read only filesystem - retrying mount RO", fsPath);
         flags |= MS_RDONLY;
-        rc = mount(fsPath, mountPoint, "ext4", flags, NULL);
+        rc = mount(fsPath, mountPoint, "ext4", flags, data);
     }
 
     return rc;
