0e1bba0867aa299fcbb4a19e73e172c7b26405b9
Flemmard <flemmard@gmail.com>
2014-12-01 18:15:38 +0100

    vold: Fix uses of board flags
    Put LOCAL_CFLAGS += AFTER LOCAL_CFLAGS := ..
    Else all flags will be removed/ignored ..
    That fixes MAX_PARTITIONS flag usage ..
    
    Change-Id: I96854b7de1eae6a235e8d31f7a9364a680b4f0b6

--- Android.mk
+++ Android.mk
@@ -69,6 +69,18 @@ common_static_libraries := \
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := libvold
+
+LOCAL_SRC_FILES := $(common_src_files)
+
+LOCAL_C_INCLUDES := $(common_c_includes)
+
+LOCAL_CFLAGS := $(common_cflags)
+
+LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
+
+LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
+
 ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
 LOCAL_CFLAGS += -DVOLD_MAX_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
 endif
@@ -81,17 +93,11 @@ ifeq ($(BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS), true)
 LOCAL_CFLAGS += -DVOLD_DISC_HAS_MULTIPLE_MAJORS
 endif
 
-LOCAL_MODULE := libvold
-
-LOCAL_SRC_FILES := $(common_src_files)
-
-LOCAL_C_INCLUDES := $(common_c_includes)
-
-LOCAL_CFLAGS := $(common_cflags)
-
-LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
-
-LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
+LOCAL_SHARED_LIBRARIES += libcryptfs_hw
+LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
+endif
 
 LOCAL_MODULE_TAGS := eng tests
 
@@ -105,11 +111,15 @@ LOCAL_SRC_FILES := vold.c
 
 LOCAL_C_INCLUDES := $(common_c_includes)
 
-LOCAL_CFLAGS := -Werror=format
+LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
+
+LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
+
+LOCAL_CFLAGS += -Werror=format
 
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
 LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
-common_shared_libraries += libcryptfs_hw
+LOCAL_SHARED_LIBRARIES += libcryptfs_hw
 LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
 endif
 
@@ -124,10 +134,6 @@ endif
 ifeq ($(BOARD_VOLD_EMMC_SHARES_DEV_MAJOR), true)
 LOCAL_CFLAGS += -DVOLD_EMMC_SHARES_DEV_MAJOR
 endif
-
-LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
-
-LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
 
 include $(BUILD_EXECUTABLE)
 
