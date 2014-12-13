084f0b3dd58e128bcd855b8f98d458a334b6a54a
Dan Albert <danalbert@google.com>
2014-12-12 17:47:16 -0800

    Move vold to GCC.
    It looks like clang might have a miscompile that is causing SIGBUS in
    `ioctl_init` when the device is encrypted. Move back to GCC until we
    can sort this out.
    
    Bug: 18736778
    Change-Id: I21ae3b9d7d9ebff8679ecc1a828b7c59f27d0903

--- Android.mk
+++ Android.mk
@@ -55,7 +55,7 @@ include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MODULE := libvold
-LOCAL_CLANG := true
+LOCAL_CLANG := false
 LOCAL_SRC_FILES := $(common_src_files)
 LOCAL_C_INCLUDES := $(common_c_includes)
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
@@ -69,7 +69,7 @@ include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MODULE:= vold
-LOCAL_CLANG := true
+LOCAL_CLANG := false
 LOCAL_SRC_FILES := \
 	main.cpp \
 	$(common_src_files)
@@ -85,7 +85,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
-LOCAL_CLANG := true
+LOCAL_CLANG := false
 LOCAL_SRC_FILES:= vdc.c
 LOCAL_MODULE:= vdc
 LOCAL_SHARED_LIBRARIES := libcutils
