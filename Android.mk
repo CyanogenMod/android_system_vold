eac6082e81b3564bd40a1b290bb709665a127f1f
Michael Bestas <mikeioannina@gmail.com>
2015-03-21 06:04:41 +0200

    vold: Fix mismerge
    Change-Id: I6c442e27bd8ea6e32dcd7961c6cb52230ff1a622

--- Android.mk
+++ Android.mk
@@ -115,14 +115,6 @@ LOCAL_SRC_FILES := vold.c
 
 LOCAL_C_INCLUDES := $(common_c_includes)
 
-LOCAL_CFLAGS := -Werror=format
-
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-LOCAL_C_INCLUDES += $(TARGET_CRYPTFS_HW_PATH)
-common_shared_libraries += libcryptfs_hw
-LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
-endif
-
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 
 LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
@@ -130,7 +122,7 @@ LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
 LOCAL_CFLAGS += -Werror=format
 
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
+LOCAL_C_INCLUDES += $(TARGET_CRYPTFS_HW_PATH)
 LOCAL_SHARED_LIBRARIES += libcryptfs_hw
 LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
 endif
