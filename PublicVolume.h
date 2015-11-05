d7b58c0a6546a5ff94db9afeeed14e115579150d
Tom Marshall <tdm@cyngn.com>
2015-11-05 11:20:54 -0800

    vold: Honor mount options for ext4 partitions
     * Save mount options and fstype for DiskPartition objects
     * Pass mount options and fstype to PublicVolume ctor
     * Pass mount options to Ext4::Mount
     * Use specified/default fstype if blkid fails
    
    This is trivially extensible for other fs types.
    
    Change-Id: Ie5dd27f26a4a7129dd9efe6a7ad0a589928282a0

--- PublicVolume.h
+++ PublicVolume.h
@@ -39,7 +39,8 @@ namespace vold {
  */
 class PublicVolume : public VolumeBase {
 public:
-    explicit PublicVolume(dev_t device, const std::string& nickname);
+    PublicVolume(dev_t device, const std::string& nickname,
+                    const std::string& mntopts = "", const std::string& fstype = "");
     virtual ~PublicVolume();
 
 protected:
@@ -73,6 +74,8 @@ private:
     std::string mFsUuid;
     /* User-visible filesystem label */
     std::string mFsLabel;
+    /* Mount options */
+    std::string mMntOpts;
 
     DISALLOW_COPY_AND_ASSIGN(PublicVolume);
 };
