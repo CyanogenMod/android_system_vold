42dd16ee22b1d27668c8cf0b8424f32693b87413
Tom Marshall <tdm@cyngn.com>
2015-11-05 11:20:54 -0800

    vold: Honor mount options for ext4 partitions
     * Save mount options and fstype for DiskPartition objects
     * Pass mount options and fstype to PublicVolume ctor
     * Pass mount options to Ext4::Mount
     * Use specified/default fstype if blkid fails
    
    This is trivially extensible for other fs types.
    
    Change-Id: Ie5dd27f26a4a7129dd9efe6a7ad0a589928282a0

--- VolumeManager.h
+++ VolumeManager.h
@@ -97,8 +97,12 @@ public:
 
     class DiskSource {
     public:
-        DiskSource(const std::string& sysPattern, const std::string& nickname, int partnum, int flags) :
-                mSysPattern(sysPattern), mNickname(nickname), mPartNum(partnum), mFlags(flags) {
+        DiskSource(const std::string& sysPattern, const std::string& nickname,
+                        int partnum, int flags,
+                        const std::string& fstype, const std::string mntopts) :
+                mSysPattern(sysPattern), mNickname(nickname),
+                mPartNum(partnum), mFlags(flags),
+                mFsType(fstype), mMntOpts(mntopts) {
         }
 
         bool matches(const std::string& sysPath) {
@@ -108,12 +112,16 @@ public:
         const std::string& getNickname() { return mNickname; }
         int getPartNum() { return mPartNum; }
         int getFlags() { return mFlags; }
+        const std::string& getFsType() { return mFsType; }
+        const std::string& getMntOpts() { return mMntOpts; }
 
     private:
         std::string mSysPattern;
         std::string mNickname;
         int mPartNum;
         int mFlags;
+        std::string mFsType;
+        std::string mMntOpts;
     };
 
     void addDiskSource(const std::shared_ptr<DiskSource>& diskSource);
