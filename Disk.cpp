8d8a9248d233fbe08566a1456a8ba864ca611250
morckx <morckx@gmail.com>
2015-11-24 22:45:01 +0100

    vold: dont't use commas in device names
    Fuse supports commas escaped by '\' in fs names, but exfat and ntfs-3g
    currently don't support the escaping.
    
    This fixes mounting of exFAT and NTFS public volumes.
    
    Change-Id: I72005489956e59e7f889267114fee3921d289984

--- Disk.cpp
+++ Disk.cpp
@@ -113,7 +113,7 @@ Disk::Disk(const std::string& eventPath, dev_t device,
         const std::string& nickname, int flags) :
         mDevice(device), mSize(-1), mNickname(nickname), mFlags(flags), mCreated(
                 false), mJustPartitioned(false) {
-    mId = StringPrintf("disk:%u,%u", major(device), minor(device));
+    mId = StringPrintf("disk:%u_%u", major(device), minor(device));
     mEventPath = eventPath;
     mSysPath = StringPrintf("/sys/%s", eventPath.c_str());
     mDevPath = StringPrintf("/dev/block/vold/%s", mId.c_str());
