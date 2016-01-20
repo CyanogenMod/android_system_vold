94ae9aea76cbee7189a1946253ba903f1d463176
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-01-20 22:06:11 +0200

    vold: Honor mount options for f2fs partitions
    * Based on 1436fc7ea3b349283a021e75fdd4f3478a2ca2e8
    
    Change-Id: Id9491ada3bd34275a95ef9855167db6ae7a9b30f

--- PublicVolume.cpp
+++ PublicVolume.cpp
@@ -170,7 +170,7 @@ status_t PublicVolume::doMount() {
         ret = ext4::Mount(mDevPath, mRawPath, false, false, true, mMntOpts,
                 false);
     } else if (mFsType == "f2fs") {
-        ret = f2fs::Mount(mDevPath, mRawPath, false);
+        ret = f2fs::Mount(mDevPath, mRawPath, mMntOpts, false);
     } else if (mFsType == "ntfs") {
         ret = ntfs::Mount(mDevPath, mRawPath, false, false, false,
                 AID_MEDIA_RW, AID_MEDIA_RW, 0007, true);
