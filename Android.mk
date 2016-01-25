55c9392260277307a80b8bad543fd7bba447289a
Edward Wang <ewang@cyngn.com>
2016-01-25 10:32:46 -0800

    build: use customized qcom device tree path if exist
    Issue-Id: YAM-87
    
    Change-Id: Ie99d4f5593315db31b94274bbfd46f229984e93e

--- Android.mk
+++ Android.mk
@@ -106,7 +106,7 @@ LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
 
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
+LOCAL_C_INCLUDES += $(call project-path-for, qcom-device-tree)/common/cryptfs_hw
 LOCAL_SHARED_LIBRARIES += libcryptfs_hw
 LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
 endif
@@ -130,7 +130,7 @@ LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
 
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
+LOCAL_C_INCLUDES += $(call project-path-for, qcom-device-tree)/common/cryptfs_hw
 LOCAL_SHARED_LIBRARIES += libcryptfs_hw
 LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
 endif
